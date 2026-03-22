405a71361af3c6ec011d56bf93d865613545a162
Abhishekmaheshwari123 <maheshwariabhishek201@gmail.com>
2026-03-22 19:53:39 +0530

    [Abhishek.Add] - Established database schema for QuantityMeasurements and QuantityMeasurementHistory in SQL. - Created repository interfaces and implementations for both in-memory and database storage.

---
+++ QuantityMeasurementSolution/QuantityMeasurement.Api/Program.cs
@@ -0,0 +1,27 @@
+using BusinessLayer.Interfaces;
+using BusinessLayer.Services;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Repositories;
+using QuantityMeasurement.Domain.Services;
+// using QuantityMeasurement.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IQuantityMeasurementRepository>(
+    QuantityMeasurementCacheRepository.Instance);
+
+builder.Services.AddScoped<IQuantityMeasurementService, QuantityMeasurementService>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
