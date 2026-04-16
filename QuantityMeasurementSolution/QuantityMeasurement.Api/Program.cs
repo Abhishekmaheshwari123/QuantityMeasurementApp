967a0021917e2eda7b3048faf551ee6211058ea7
Abhishekmaheshwari123 <114015344+Abhishekmaheshwari123@users.noreply.github.com>
2026-04-16 12:58:17 +0530

    Update CORS policy to ensure consistent use of HTTPS for frontend origin

--- QuantityMeasurementSolution/QuantityMeasurement.Api/Program.cs
+++ QuantityMeasurementSolution/QuantityMeasurement.Api/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "https://quantitymeasurementapp-frontend-po29.onrender.com",
-                "http://quantitymeasurementapp-frontend-po29.onrender.com"
+                "https://quantitymeasurementapp-frontend-po29.onrender.com"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
